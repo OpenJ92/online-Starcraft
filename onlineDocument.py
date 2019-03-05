5d29ed944be7997419aa417a06b2bdfe761eb92c
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-03-05 14:23:48 -0500

    Introduction of virtualenv for project. Ignore directory in gitignore. Look to add a requirement.txt file to directory to capture a low memory representaion of _env/ directory

--- onlineDocument.py
+++ onlineDocument.py
@@ -9,12 +9,6 @@ class Document():
     def __init__(self):
         pass
 
-    def construct_document(self):
-        self.load_replay()
-
-    def load_replay(self):
-        pass
-
     # we want to construct documents out of our Starcraft 2 replays as a means
     # to perform ML NLP analysis. LSTM, clustering etc...
     # We're looking to apply Word to Vec and n-gram prediction to the replays
