e49af66c72866e30603963a8fbabc77e9550680b
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-07-14 22:41:12 -0400

    SVD module started. Look to push SC2 data through ML model on a weekly basis.

--- MachineLearning/PCA_Game.py
+++ MachineLearning/PCA_Game.py
@@ -3,19 +3,28 @@ from onlineDB.database.replays.onlineModels import db
 from onlineDB.database.replays.onlineModels import Participant, User, Game, PlayerStatsEvent, UnitBornEvent, \
                          UnitTypeChangeEvent, UpgradeCompleteEvent, UnitDoneEvent, \
                          BasicCommandEvent, TargetPointCommandEvent, UnitDiedEvent, UnitInitEvent
+from onlineDB.database.FSV.onlineModels import db as FSV_db
+from onlineDB.database.FSV.onlineModels import FSV
 from onlineDB.HyperSphere.resolveDirection import resolveDirection
-from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+from scipy.linalg import svd
 
-class PCA_:
-    def __init__(self, participant, event_name):
+class SVD:
+    def __init__(self, participant, event_name, db_):
         self.participant = participant
+        self.db = db_
         self.table = Table(self.participant, event_name)
-        self.pca = self.FSV()
+        self.tSVD = self.tSVD()
 
-    def FSV(self):
-        pca = PCA(copy = False)
-        return pca.fit(self.table.table.values)
+    def tSVD(self):
+        U, s_, VT = svd(self.table.create_agg_table().values)
+        import pdb;pdb.set_trace()
+        return 0
+
+    def database_inject(self):
+        components_ = self.tSVD.components_[:,0].tobytes()
+        pass
 
 if __name__ == "__main__":
     p = db.session.query(Participant)[20]
-    pca = PCA_(p, "UBE")
+    pca = SVD(p, "UBE", FSV_db)
--- ScrapeReplay/run_scraper.sh
+++ ScrapeReplay/run_scraper.sh
@@ -4,6 +4,6 @@ source onlineDB/ONLINESTARCRAFT/bin/activate
 
 date_of_execution="$(date)"
 touch onlineDB/ScrapeReplay/scrape_logs/"${date_of_execution}".txt
-$(which python) onlineDB/ScrapeReplay/runonlineDB.py > onlineDB/ScrapeReplay/scrape_logs/"${date_of_execution}".txt
+$(which ipython) onlineDB/ScrapeReplay/runonlineDB.py > onlineDB/ScrapeReplay/scrape_logs/"${date_of_execution}".txt
 
 deactivate
